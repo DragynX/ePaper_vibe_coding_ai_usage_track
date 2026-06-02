237f912a499e21704296650e01abd65462279805
limengdu <747632169@qq.com>
2026-06-02 17:23:24 +0800

    fix: vertically align dashboard header

--- src/UsageUI.cpp
+++ src/UsageUI.cpp
@@ -241,14 +241,14 @@ void UsageUI::drawHeader(const UiStatus& st, long nowEpoch) {
   const int w = display_.width();
   const int margin = kIsLarge ? 44 : 24;
   const int topY = kIsLarge ? 14 : 12;
+  const int headerLineY = kIsLarge ? topY + 94 : 0;
+  const int headerMiddleY = kIsLarge ? topY + 44 : topY;
   const int titleSize = kIsLarge ? 4 : 3;
 
   // Left: app title.
   if (kIsLarge) {
-    const int laneW = (w - margin * 2) / 3;
-    const int titleCenterX = margin + laneW / 2;
-    renderer_.drawTextFace(uiStr(UiStringId::kAppName), titleCenterX, topY,
-                           TextFace::SansBold24, TextAlign::TopCenter, kText, kBg);
+    renderer_.drawTextFace(uiStr(UiStringId::kAppName), margin, headerMiddleY,
+                           TextFace::SansBold24, TextAlign::MiddleLeft, kText, kBg);
   } else {
     renderer_.drawText(uiStr(UiStringId::kAppName), margin, topY, titleSize,
                        TextAlign::TopLeft, kText, kBg);
@@ -281,21 +281,20 @@ void UsageUI::drawHeader(const UiStatus& st, long nowEpoch) {
   const int wifiW = kIsLarge ? 42 : 30;
   const int wifiH = kIsLarge ? 30 : 22;
   if (kIsLarge) {
-    const int laneW = (w - margin * 2) / 3;
-    const int rightLaneCenterX = margin + laneW * 2 + laneW / 2;
     const int noteW = renderer_.measureTextFace(uiStr(UiStringId::kRefreshNote),
                                                 TextFace::SansBold9);
     const int groupGap = 18;
     const int groupW = noteW + groupGap + wifiW;
-    const int groupX = rightLaneCenterX - groupW / 2;
-    renderer_.drawTextFace(uiStr(UiStringId::kRefreshNote), groupX, topY + 3,
-                           TextFace::SansBold9, TextAlign::TopLeft, kText, kBg);
-    drawWifiIcon(groupX + noteW + groupGap, topY, wifiW, wifiH, st.wifiConnected, kText);
+    const int groupX = w - margin - groupW;
+    renderer_.drawTextFace(uiStr(UiStringId::kRefreshNote), groupX, headerMiddleY,
+                           TextFace::SansBold9, TextAlign::MiddleLeft, kText, kBg);
+    drawWifiIcon(groupX + noteW + groupGap, headerMiddleY - wifiH / 2,
+                 wifiW, wifiH, st.wifiConnected, kText);
     if (st.batteryPercent >= 0) {
-      drawBatteryIcon(rightLaneCenterX - 28, topY + wifiH + 14, 56, 26,
+      drawBatteryIcon(w - margin - 56, headerLineY - 34, 56, 26,
                       st.batteryPercent, kText);
     }
-    display_.fillRect(margin, topY + 94, w - margin * 2, 2, kLine);
+    display_.fillRect(margin, headerLineY, w - margin * 2, 2, kLine);
   } else {
     drawWifiIcon(w - margin - wifiW, topY, wifiW, wifiH, st.wifiConnected, kText);
   }
